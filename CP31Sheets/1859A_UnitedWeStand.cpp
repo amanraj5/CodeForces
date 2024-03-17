4228aa36da8b1032e875c7daabc771b3b20122e6
Aman Raj <AMANRAJSINGH806@GMAIL.COM>
2024-03-17 15:43:27 +0530

    problem13

---
+++ CP31Sheets/1859A_UnitedWeStand.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        sort(a.begin(), a.end());
+        if (a[0] == a.back())
+        {
+            cout << -1 << endl;
+        }
+        else
+        {
+            int i = 0;
+            while (a[i] == a[0])
+                i++;
+            cout << i << " " << n - i << endl;
+            for (int j = 0; j < i; j++)
+                cout << a[j] << " ";
+            for (int j = i; j < n; j++)
+                cout << a[j] << " ";
+        }
+    }
+    return 0;
+}
