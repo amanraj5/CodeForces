b56fb06098cbafa10df2efe140f90b9f64de5792
Aman Raj <AMANRAJSINGH806@GMAIL.COM>
2024-05-23 13:24:18 +0530

    Minimum LCM

---
+++ CP31Sheets(1000)/1765M_MinimumLCM.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int ans = 1;
+        for (int i = 2; i * i <= n; i++)
+        {
+            if (n % i == 0)
+            {
+                ans = n / i;
+                break;
+            }
+        }
+        cout << ans << " " << n - ans << endl;
+    }
+    return 0;
+}
