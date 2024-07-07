fa0f08769322843bf528f5d05fe0de1ecc3a0dd2
Aman Raj <AMANRAJSINGH806@GMAIL.COM>
2024-07-07 12:34:03 +0530

    BuyingTorches

--- CP31Sheets(1000)/1418A_BuyingTorches.cpp
+++ CP31Sheets(1000)/1418A_BuyingTorches.cpp
@@ -1,14 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
 int main()
 {
     int t;
     cin >> t;
     while (t--)
     {
-        int x, y, k;
+        ll x, y, k;
         cin >> x >> y >> k;
-        
+        ll ans = (k * y + k - 1 + x - 1 - 1) / (x - 1);
+        // For ceil division a/b=(a+b-1)/b, where a=(k*y+k-1),b=(x-1);
+        cout << ans + k << endl;
     }
     return 0;
 }
